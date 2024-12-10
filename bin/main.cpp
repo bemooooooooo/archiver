dda406793849671b850ad719fe01d0fcd8fe9aa6
ki_mi_oshi <103956922+bemooooooooo@users.noreply.github.com>
2024-12-10 13:49:16 +0500

    Create main.cpp

---
+++ bin/main.cpp
@@ -0,0 +1,9 @@
+#include <lib/ArgParser/ArgParser.h>
+#include <lib/HamArchiver/HamArchiver.h>
+#include <runtime/runtime.cpp>
+#include <bitset>
+
+int main(int argc, char** argv) {
+  runtime(argc, argv);
+  return 0;
+}
